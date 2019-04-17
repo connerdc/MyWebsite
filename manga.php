defbb861a0a049fb9e502c57454bc3fd59990131
Conner Christopherson <cdcqq7@gmail.com>
2019-04-17 18:26:22 -0500

    multiple tables now are supported, status based on form and error checked in form

--- manga.php
+++ manga.php
@@ -4,6 +4,11 @@
     include 'php/db_connection.php';
     $conn = OpenCon();
 ?>
+
+<!-- Features needed:
+    1. Tables for completed, plan to read, etc. NEED TO MAKE SURE WE CHANGE ALL PHP FILES THAT DEPENDS ON A NEW SQL COLUMN
+-->
+
 <head>
     <meta charset="utf-8">
 
@@ -31,7 +36,6 @@
                     number += parse[increment];
                     increment++;
                 }                
-                console.log(number);
                 var mangaID = parseInt(number);
                 $.ajax({
                     type: "POST",
@@ -73,6 +77,7 @@
                         $("#edit-manga-completed").val(data.completedCptr);
                         $("#edit-manga-total").val(data.totalCptr);
                         $("#edit-manga-score").val(data.score);
+                        $("#edit-manga-status").val(data.status);
                         $("#edit-manga-thoughts").val(data.thoughts);
                     },
                     error: function (data) {
@@ -94,6 +99,7 @@
                         completedCptr: $("#edit-manga-completed").val(),
                         totalCptr: $("#edit-manga-total").val(),
                         score: $("#edit-manga-score").val(),
+                        status: $("#edit-manga-status").val(),
                         thoughts: $("#edit-manga-thoughts").val()
                     },
                     success: function (response) {
@@ -122,6 +128,7 @@
                         completedCptr: $("#manga-completed").val(),
                         totalCptr: $("#manga-total").val(),
                         score: $("#manga-score").val(),
+                        status: $("#manga-status").val(),
                         thoughts: $("#manga-thoughts").val()
                     },
                     success: function (response) {
@@ -191,8 +198,6 @@
 
         <div id="contentformat">
             <div class="container">
-                <button type="button" id="newMangaBtn" class="btn btn-dark" data-toggle="modal" data-target=".newmanga">Add Manga</button>
-
                 <div id="modal" class="modal fade newmanga" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
                     <div class="modal-dialog modal-lg">
                         <div class="modal-content">
@@ -237,6 +242,18 @@
                                                 <option value="3">3 (Bad)</option>
                                                 <option value="2">2 (Awful)</option>
                                                 <option value="1">1 (Dumpster Fire)</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <div class="col-4">
+                                            <label>Status</label>
+                                            <select name="status" id="manga-status" class="form-control">
+                                                <option selected>Choose...</option>
+                                                <option value="plantoread">Plan to Read</option>
+                                                <option value="reading">Reading</option>
+                                                <option value="completed">Completed</option>
+                                                <option value="dropped">Dropped</option>
                                             </select>
                                         </div>
                                     </div>
@@ -300,6 +317,18 @@
                                             </select>
                                         </div>
                                     </div>
+                                    <div class="form-group row">
+                                        <div class="col-4">
+                                            <label>Status</label>
+                                            <select name="status" id="edit-manga-status" class="form-control">
+                                                <option selected>Choose...</option>
+                                                <option value="plantoread">Plan to Read</option>
+                                                <option value="reading">Reading</option>
+                                                <option value="completed">Completed</option>
+                                                <option value="dropped">Dropped</option>
+                                            </select>
+                                        </div>
+                                    </div>
                                     <div class="form-group">
                                         <label>Thoughts</label>
                                         <textarea name="thoughts" id="edit-manga-thoughts" class="form-control" rows="5" id="comment"></textarea>
@@ -312,7 +341,14 @@
                     </div>
                 </div>
 
-
+                <div class="row">
+                    <div class="col-6">
+                        <h1 id="specialHeader" class="tableheader">Reading</h1>
+                    </div>
+                    <div id="alignaddmanga" class="col-6">
+                        <button type="button" id="newMangaBtn" class="btn btn-dark" data-toggle="modal" data-target=".newmanga">Add Manga</button>
+                    </div>
+                </div>
                 <table class="table table-striped table-sm table-bordered">
                     <thead class="thead-dark">
                         <tr>
@@ -328,9 +364,8 @@
                     </thead>
                     <tbody>
                         <?php
-                            $sql = "SELECT * FROM manga";
+                            $sql = "SELECT * FROM manga WHERE `status`='reading'";
                             $result = mysqli_query($conn, $sql);
-                            $number = 1;
 
                             if(mysqli_num_rows($result) > 0) {
                                 while($row = mysqli_fetch_assoc($result)) {
@@ -338,27 +373,122 @@
                                     <td class=\"tableopacity\">" . $row["name"]. "</td>
                                     <td id=\"specalign\" class=\"tableopacity\"><a href=\"" . $row["link"] . "\">Link</a></td>
                                     <td id=\"specalign\" class=\"tableopacity\">" . $row["completedCptr"] . "/" . $row["totalCptr"] . "</td>
-                                    <td class=\"pluscol tableopacity\"><button id=\"" . $number . "button\" class=\"btn plusbutton tableopacity\">+</button> </td>
+                                    <td class=\"pluscol tableopacity\"><button id=\"" . $row["mangaID"] . "button\" class=\"btn plusbutton tableopacity\">+</button> </td>
                                     <td id=\"specalign\" class=\"tableopacity\">" . $row["score"]. "</td>
                                     <td class=\"tableopacity\">" . $row["thoughts"]. "</td>
-                                    <td id=\"" . $number . "edit\" class=\"tableopacity editbtn\"><button class=\"btn btn-dark\" data-toggle=\"modal\" data-target=\".editmanga\">Edit</button>
+                                    <td id=\"" . $row["mangaID"] . "edit\" class=\"tableopacity editbtn\"><button class=\"btn btn-dark\" data-toggle=\"modal\" data-target=\".editmanga\">Edit</button>
                                     </tr>";
-
-                                    $number = $number + 1;
                                 }
                             }
                         ?>
-                        <!-- <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td><a href="#">Link</a></td>
-                            <td id="specalign">78/149</td>
-                            <td><button class="plusminusbtn">+</button></td>
-                            <td><button class="plusminusbtn">-</button></td>
-                            <td id="specalign">10</td>
-                            <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</td>
-                            <td id="editbtn"><button>Edit</button></td> 
-                        </tr> -->
+                    </tbody>
+                </table>
+
+                <h1 id="specialHeader" class="tableheader">Completed</h1>
+                <table class="table table-striped table-sm table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th style="width: 5%">#</th>
+                            <th style="width: 35%">Name</th>
+                            <th style="width: 3%">Link</th>
+                            <th style="width: 5%">Progress</th>
+                            <th style="width: 3%">+</th>
+                            <th style="width: 4%">Score</th>
+                            <th style="width: 40%">Thoughts</th>
+                            <th style="width: 5%">Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM manga WHERE `status`='completed'";
+                            $result = mysqli_query($conn, $sql);
+
+                            if(mysqli_num_rows($result) > 0) {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr><th id=\"specalign\" class=\"tableopacity\" scope=\"row\">" . $row["mangaID"] . "</th>
+                                    <td class=\"tableopacity\">" . $row["name"]. "</td>
+                                    <td id=\"specalign\" class=\"tableopacity\"><a href=\"" . $row["link"] . "\">Link</a></td>
+                                    <td id=\"specalign\" class=\"tableopacity\">" . $row["completedCptr"] . "/" . $row["totalCptr"] . "</td>
+                                    <td class=\"pluscol tableopacity\"><button id=\"" . $row["mangaID"] . "button\" class=\"btn plusbutton tableopacity\">+</button> </td>
+                                    <td id=\"specalign\" class=\"tableopacity\">" . $row["score"]. "</td>
+                                    <td class=\"tableopacity\">" . $row["thoughts"]. "</td>
+                                    <td id=\"" . $row["mangaID"] . "edit\" class=\"tableopacity editbtn\"><button class=\"btn btn-dark\" data-toggle=\"modal\" data-target=\".editmanga\">Edit</button>
+                                    </tr>";
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
+
+                <h1 id="specialHeader" class="tableheader">Dropped</h1>
+                <table class="table table-striped table-sm table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th style="width: 5%">#</th>
+                            <th style="width: 35%">Name</th>
+                            <th style="width: 3%">Link</th>
+                            <th style="width: 5%">Progress</th>
+                            <th style="width: 3%">+</th>
+                            <th style="width: 4%">Score</th>
+                            <th style="width: 40%">Thoughts</th>
+                            <th style="width: 5%">Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM manga WHERE `status`='dropped'";
+                            $result = mysqli_query($conn, $sql);
+
+                            if(mysqli_num_rows($result) > 0) {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr><th id=\"specalign\" class=\"tableopacity\" scope=\"row\">" . $row["mangaID"] . "</th>
+                                    <td class=\"tableopacity\">" . $row["name"]. "</td>
+                                    <td id=\"specalign\" class=\"tableopacity\"><a href=\"" . $row["link"] . "\">Link</a></td>
+                                    <td id=\"specalign\" class=\"tableopacity\">" . $row["completedCptr"] . "/" . $row["totalCptr"] . "</td>
+                                    <td class=\"pluscol tableopacity\"><button id=\"" . $row["mangaID"] . "button\" class=\"btn plusbutton tableopacity\">+</button> </td>
+                                    <td id=\"specalign\" class=\"tableopacity\">" . $row["score"]. "</td>
+                                    <td class=\"tableopacity\">" . $row["thoughts"]. "</td>
+                                    <td id=\"" . $row["mangaID"] . "edit\" class=\"tableopacity editbtn\"><button class=\"btn btn-dark\" data-toggle=\"modal\" data-target=\".editmanga\">Edit</button>
+                                    </tr>";
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
+
+                <h1 id="specialHeader" class="tableheader">Plan-to-Read</h1>
+                <table class="table table-striped table-sm table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th style="width: 5%">#</th>
+                            <th style="width: 35%">Name</th>
+                            <th style="width: 3%">Link</th>
+                            <th style="width: 5%">Progress</th>
+                            <th style="width: 3%">+</th>
+                            <th style="width: 4%">Score</th>
+                            <th style="width: 40%">Thoughts</th>
+                            <th style="width: 5%">Edit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM manga WHERE `status`='plantoread'";
+                            $result = mysqli_query($conn, $sql);
+
+                            if(mysqli_num_rows($result) > 0) {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    echo "<tr><th id=\"specalign\" class=\"tableopacity\" scope=\"row\">" . $row["mangaID"] . "</th>
+                                    <td class=\"tableopacity\">" . $row["name"]. "</td>
+                                    <td id=\"specalign\" class=\"tableopacity\"><a href=\"" . $row["link"] . "\">Link</a></td>
+                                    <td id=\"specalign\" class=\"tableopacity\">" . $row["completedCptr"] . "/" . $row["totalCptr"] . "</td>
+                                    <td class=\"pluscol tableopacity\"><button id=\"" . $row["mangaID"] . "button\" class=\"btn plusbutton tableopacity\">+</button> </td>
+                                    <td id=\"specalign\" class=\"tableopacity\">" . $row["score"]. "</td>
+                                    <td class=\"tableopacity\">" . $row["thoughts"]. "</td>
+                                    <td id=\"" . $row["mangaID"] . "edit\" class=\"tableopacity editbtn\"><button class=\"btn btn-dark\" data-toggle=\"modal\" data-target=\".editmanga\">Edit</button>
+                                    </tr>";
+                                }
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
